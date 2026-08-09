444df668460d806936008b92322f88ad3f105f90
zourit77 <29767562+zourit77@users.noreply.github.com>
2026-08-09 22:24:38 +0200

    Update nixpkgs URL to version 26.05

--- pizero2w/flake.nix
+++ pizero2w/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS RPi Zero 2 W (Docker)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs, ... }:
